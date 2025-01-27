cf1293bb4af029da1e607aee0d05aa84c73ecc8e
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-27 20:51:17 +0530

    Update qualityGate.groovy

--- vars/qualityGate.groovy
+++ vars/qualityGate.groovy
@@ -1,8 +1,15 @@
+// def call(Map params = [:]) {
+//     def credentialsId = params.get('credentialsId', 'sonar-token')
+//     def abortPipeline = params.get('abortPipeline', false)
+
+//     script {
+//         waitForQualityGate abortPipeline: abortPipeline, credentialsId: credentialsId
+//     }
+// }
 def call(Map params = [:]) {
     def credentialsId = params.get('credentialsId', 'sonar-token')
     def abortPipeline = params.get('abortPipeline', false)
 
-    script {
-        waitForQualityGate abortPipeline: abortPipeline, credentialsId: credentialsId
-    }
+    waitForQualityGate abortPipeline: abortPipeline, credentialsId: credentialsId
 }
+
