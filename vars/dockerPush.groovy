417ac32900ccff55c053e1fbdf46d2f32337eac0
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-27 14:00:00 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,9 +1,7 @@
-def call() {
-    script {
-        withCredentials([usernamePassword(credentialsId: 'dockerHub-cred', passwordVariable: 'docker_pass', usernameVariable: 'docker_user')]) {
-            sh "docker login -u ${docker_user} -p ${docker_pass}"
-            sh "docker push ${IMAGE_NAME}:v${BUILD_NUMBER}"
-            sh "docker push ${IMAGE_NAME}:latest"
-        }
+def call(String imageName, String buildNumber) {
+    withCredentials([usernamePassword(credentialsId: 'dockerHub-cred', passwordVariable: 'docker_pass', usernameVariable: 'docker_user')]) {
+        sh "docker login -u ${docker_user} -p ${docker_pass}"
+        sh "docker push ${imageName}:${buildNumber}"
+        sh "docker push ${imageName}:latest"
     }
 }
