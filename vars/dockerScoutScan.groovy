31ef063f620f3a7db25f802abef6c4ca2e0cc8d3
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-27 16:24:09 +0530

    Update dockerScoutScan.groovy

--- vars/dockerScoutScan.groovy
+++ vars/dockerScoutScan.groovy
@@ -1,11 +1,3 @@
-// def call(String imageName) {
-//     withCredentials([usernamePassword(credentialsId: 'dockerHub-cred', passwordVariable: 'docker_pass', usernameVariable: 'docker_user')]) {
-//         sh "docker login -u ${docker_user} -p ${docker_pass}"
-//         sh "docker-scout quickview ${imageName}:latest"
-//         sh "docker-scout cves ${imageName}:latest"
-//         sh "docker-scout recommendations ${imageName}:latest"
-//     }
-// }
 def call(String imageName) {
     def imageTag = "v${BUILD_NUMBER}"
     withCredentials([usernamePassword(credentialsId: 'dockerHub-cred', passwordVariable: 'docker_pass', usernameVariable: 'docker_user')]) {
