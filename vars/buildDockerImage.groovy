0ab51098d259186564905e015e1f5c39a6e7384a
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-27 14:01:13 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,8 +1,3 @@
-def call(Map params = [:]) {
-    def dockerImageName = params.get('dockerImageName', "${JOB_NAME_LOWER}")
-    def dockerTag = params.get('dockerTag', "${BUILD_NUMBER}")
-    
-    script {
-        sh "docker build -t ${dockerImageName}:${dockerTag} ."
-    }
+def call(String imageName, String buildNumber) {
+    sh "docker build -t ${imageName}:${buildNumber} ."
 }
