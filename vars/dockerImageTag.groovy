03a7ac6edab034f1b63db8b7708854900f4100a8
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-27 14:23:43 +0530

    Update dockerImageTag.groovy

--- vars/dockerImageTag.groovy
+++ vars/dockerImageTag.groovy
@@ -2,3 +2,8 @@ def call() {
     sh "docker tag ${IMAGE_TAG} ${IMAGE_NAME}:v${BUILD_NUMBER}"
     sh "docker tag ${IMAGE_TAG} ${IMAGE_NAME}:latest"
 }
+def call(String JOB_NAME, int BUILD_NUMBER, String docker_user) {
+    sh """
+        docker tag ${JOB_NAME}:v${BUILD_NUMBER} ${docker_user}/${JOB_NAME}:latest
+    """
+}
