1f658b844df57b6ac75e8e3214b9896b9bd8b0a4
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-27 15:30:28 +0530

    Update dockerImageTag.groovy

--- vars/dockerImageTag.groovy
+++ vars/dockerImageTag.groovy
@@ -1,3 +1,4 @@
+@NonCPS
 def call(String imageTag, String imageName, int buildNumber) {
     sh "docker tag ${imageTag} ${imageName}:v${buildNumber}"
     sh "docker tag ${imageTag} ${imageName}:latest"
