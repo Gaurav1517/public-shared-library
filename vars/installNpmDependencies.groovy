3447503ddf026c3cdb3ef75caf9ba493bb692b80
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-28 11:22:23 +0530

    Update installNpmDependencies.groovy

--- vars/installNpmDependencies.groovy
+++ vars/installNpmDependencies.groovy
@@ -1,6 +1,6 @@
 def call() {
-    sh 'npm install -g npm@latest'
-    sh 'npm cache clean --force'
-    sh 'rm -rf node_modules'
+    // sh 'npm install -g npm@latest'
+    // sh 'npm cache clean --force'
+    // sh 'rm -rf node_modules'
     sh 'npm install'
 }
