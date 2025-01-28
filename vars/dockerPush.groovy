e0024dce89aa06edba9a92924e778a0863ea92cb
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-28 15:23:43 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -8,9 +8,9 @@
 def call(String imageName, String buildNumber) {
     withCredentials([usernamePassword(credentialsId: 'dockerHub-cred', passwordVariable: 'docker_pass', usernameVariable: 'docker_user')]) {
         // Validate image name and build number
-        if (!imageName?.trim() || !buildNumber?.trim()) {
-            error "Invalid image name or build number: ${imageName}:${buildNumber}"
-        }
+        // if (!imageName?.trim() || !buildNumber?.trim()) {
+        //     error "Invalid image name or build number: ${imageName}:${buildNumber}"
+        // }
         sh "echo ${docker_pass} | docker login -u ${docker_user} --password-stdin"
         sh "docker push ${imageName}:latest"
     }
